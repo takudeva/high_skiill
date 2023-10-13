b5ecae6ea540a758407417072bc16f8ece4fc5a9
takudeva <126333608+takudeva@users.noreply.github.com>
2023-10-13 22:31:14 +0000

    [Edit6]start making controller homes top

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,8 @@
 class HomesController < ApplicationController
   def top
+    @question = ChineseCharacter.find_by(id: params[:chinese_character][:level_of_chinese_character])
+    params[:group] = 1
+    params[:page] = 1
   end
 
   def about
