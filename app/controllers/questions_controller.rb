937ad12606ec9bd4f256349eeb81c2e9e5f4fa08
takudeva <126333608+takudeva@users.noreply.github.com>
2023-10-14 10:04:36 +0000

    [Edit8]in making controller questions show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,22 @@
 class QuestionsController < ApplicationController
   def show
-  # パラメータを受け取り、変数id,type,group,pageに代入する
-    id = ChineseCharacter.find_by!(level_of_chinese_character: params[:id])
+  # パラメータを受け取り、変数level,type,group,pageに代入する
+    level = params[:level]
     type = params[:type]
     group = params[:group]
     page = params[:page]
 
-    # group == 1..4について、次の場合分けを行う
+    # group == 1..3について、次の場合分けを行う
     # group == 1 => number_for_each_level: 1..20
     # group == 2 => number_for_each_level: 21..40
     # group == 3 => number_for_each_level: 41..60
     # 場合分けと取得したgroupの値に基づき、numberを制限する
     # 制限された値をquestions_with_limited_numに代入する
-    case group
-    when 1
+    if group == 1
       questions_with_limited_num = ChineseCharacter.where(number_for_each_level: 1..20)
-    when 2
+    elsif group == 2
       questions_with_limited_num = ChineseCharacter.where(number_for_each_level: 21..40)
-    when 3
+    elsif group == 3
       questions_with_limited_num = ChineseCharacter.where(number_for_each_level: 41..60)
     # else
     #   render json: { error: "Invalid group parameter" }, status: :bad_request
@@ -26,7 +25,7 @@ class QuestionsController < ApplicationController
 
     # questions_with_limited_numを、取得したid(level_of_chinese_character)に基づき、レベルも制限する
     # 制限された値をquestions_with_limited_num_and_levelに代入する
-    questions_with_limited_num_and_level = questions_with_limited_num.where(level_of_chinese_character: id, group: group)
+    questions_with_limited_num_and_level = questions_with_limited_num.where(level_of_chinese_character: level, group: group)
 
     # questions_with_limited_num_and_levelを、取得したtypeの値に基づき、場合分けする
     # type == read => :reading_of_chinese_characterをselectしread_questionsに代入
@@ -39,13 +38,12 @@ class QuestionsController < ApplicationController
       page = [1, page, 4].sort[1]
       @questions = selected_read_questions
       @page = page
-    else type == "mean"
+    elsif type == "mean"
       mean_questions = questions_with_limited_num_and_level.select(:chinese_character, :meaning_of_chinese_character)
       selected_mean_questions = mean_questions.order("RANDOM()").limit(5)
       page = [1, page, 4].sort[1]
-      @questions = selected_read_questions
+      @questions = selected_mean_questions
       @page = page
-
     # else
       # # render json: { error: "Invalid type parameter" }, status: :bad_request
       # return
