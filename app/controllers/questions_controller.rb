983acbf3f3497c39daf384107c4470af0f7e53bd
takudeva <126333608+takudeva@users.noreply.github.com>
2023-10-21 12:59:54 +0000

    [Edit16]in making questions result

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,122 +1,105 @@
 class QuestionsController < ApplicationController
+
   def show
-    @correct = Correct.new
-
   # パラメータを受け取り、変数level,type,group,pageに代入する
-    @level = params[:level]
-    @type = params[:type]
-    @group = params[:group]
-    @page = params[:page]
-
-    # group == 1..3について、次の場合分けを行う
-    # group == 1 => number_for_each_level: 1..20
-    # group == 2 => number_for_each_level: 21..40
-    # group == 3 => number_for_each_level: 41..60
-    # 場合分けと取得したgroupの値に基づき、numberを制限する
-    # 制限された値をquestions_with_limited_numに代入する
-    questions_with_limited_num = []
-    if @group.to_i == 1
-      questions_with_limited_num = ChineseCharacter.where(number_for_each_level: 1..20)
-    elsif @group.to_i == 2
-      questions_with_limited_num = ChineseCharacter.where(number_for_each_level: 21..40)
-    elsif @group.to_i == 3
-      questions_with_limited_num = ChineseCharacter.where(number_for_each_level: 41..60)
-    else
-      # エラーハンドリング: 無効なパラメータ
-      flash[:error] = "有効なパラメータではありません。"
-    end
-
-    # 同じグループ内の20問の問題を5問ずつ、4ページに分けて表示
-    questions_per_page = 5
-    @total_pages = (questions_with_limited_num.count / questions_per_page)
-    @current_page = @page.to_i
-    offset = (@current_page - 1) * questions_per_page
-
-    # ページが範囲外の場合はリダイレクト
-    if @current_page < 1 || @current_page > @total_pages
-      flash[:error] = "ページが存在しません。"
-    end
-
-    # questions_with_limited_numを、取得したid(level_of_chinese_character)に基づき、レベルも制限する
-    # 制限された値をquestions_with_limited_num_and_levelに代入する
-    questions_with_limited_num_and_level =  questions_with_limited_num.where(level_of_chinese_character: @level)
-    # 以前のページで出題された問題を除外
-    previous_questions = session[:previous_questions] || []
-    questions_with_limited_num_and_level = questions_with_limited_num_and_level.where.not(id: previous_questions)
-
-    # @questionsから無作為に(order("RANDOM()"))、5問のみ(limit)抽出して@question_setに代入する
-    @question_set = questions_with_limited_num_and_level.order("RANDOM()").limit(5)
-
-    # まず、@questions_with_choicesに空の配列(箱)[]を代入する
-    # question_setに含まれるquestionそれぞれ(.each)に対し、choicesを用意する
-    # @questions_with_choicesにオブジェクト(値はquestion,choices)を追加する
-    # generate_choicesはprivateの中で定義する
-    # 出した問題のid(question.id)をsession[:question_history]配列に保存することで、回答履歴を保持する
-    @questions_with_choices = []
-    @question_set.each do |question|
-      choices = generate_choices(question, @type)
-      @questions_with_choices << { question: question, choices: choices }
-      previous_questions << question.id
-    end
-
-    if flash[:error]
-      redirect_to root_path
-      return
-    end
-
+    level = params[:level]
+    type = params[:type]
+    group = params[:group]
+    page = params[:page]
+    generate_quiz(group, page, level, type)
+    @type = type
   end
 
   def answer
-    @level = params[:level]
-    @type = params[:type]
+    level = params[:level]
+    type = params[:type]
       # indexとして取得した値("#{qwc[:question].id}_#{choice}")を5問分("0"-"4")、"_"を境に切り離し、前者をquestion_idに、後者をselected_choiceに代入
       # ChineseCharacterモデルからquestion_idと同じid、かつ、levelが受け取ったパラメータと同じものを探し、ローカル変数questionに代入
       # correct_choiceにquestion_idとlevelが一致する読み(question.reading_of_chinese_character)を代入しておく
       # selected_choice == correct_choiceかを判定 => 真の場合、is_correctに"true"代入、偽は"false"代入
       # @correct.correct_of_readingにはis_correct => 真偽値が代入される
-    ["0","1","2","3","4"].each do |i|
+    @answers =[]
+    [0,1,2,3,4].each do |i|
       correct = Correct.new
-      question_id, selected_choice = params[i].split("_")
-      question = ChineseCharacter.find_by!(id: question_id, level_of_chinese_character: @level)
-      if @type == "read"
-        correct_choice = question.reading_of_chinese_character
-        correct_choice == selected_choice ? is_correct = "true" : is_correct = "false"
+      question_id = params[:chinese_character_of_question][i]
+      selected_id, selected_choice = params[i.to_s].split("_")
+      question = ChineseCharacter.find_by!(id: question_id, level_of_chinese_character: level)
+      if type == "read"
+        #correct_choice = question.reading_of_chinese_character
+        question_id == selected_id ? is_correct = "true" : is_correct = "false"
         correct.correct_of_reading = is_correct
-      elsif @type == "mean"
-        correct_choice = question.meaning_of_chinese_character
-        correct_choice == selected_choice ? is_correct = "true" : is_correct = "false"
+      elsif type == "mean"
+        #correct_choice = question.meaning_of_chinese_character
+        question_id == selected_id  ? is_correct = "true" : is_correct = "false"
         correct.correct_of_meaning = is_correct
       end
       correct.user_id = current_user.id
       correct.chinese_character_id = question.id
       correct.save!
+      @answers << correct
     end
     flash[:success] = "解答しました"
-    redirect_to answer_questions_path
+    result
+    #@answer.each do | answer |
+    #  pp answer
+    #  pp answer.chinese_character.chinese_character
+    #  pp answer.correct_of_reading
+    #end
+    #redirect_to answer_questions_path
 
   end
 
   def result
-    @type = params[:type]
+    level = params[:level]
+    type = params[:type]
+    group = params[:group]
+    page = params[:page]
+    #generate_quiz(group, page, level, type)
+    @type = type
+    render :result
   end
 
   def score
   end
 
   private
+  def generate_quiz(group, page, level, type)
+
+    questions_with_limited_num = []
+    if group.to_i == 1
+      questions_with_limited_num = ChineseCharacter.where(number_for_each_level: 1..20)
+    elsif group.to_i == 2
+      questions_with_limited_num = ChineseCharacter.where(number_for_each_level: 21..40)
+    elsif group.to_i == 3
+      questions_with_limited_num = ChineseCharacter.where(number_for_each_level: 41..60)
+    else
+      flash[:error] = "有効なパラメータではありません。"
+    end
+
+    questions_per_page = 5
+    total_pages = (questions_with_limited_num.count / questions_per_page)
+    current_page = page.to_i
+    offset = (current_page - 1) * questions_per_page
+    if current_page < 1 || current_page > total_pages
+      flash[:error] = "ページが存在しません。"
+    end
+
+    previous_questions = session[:previous_questions] || []
+    questions_with_limited_num_and_level = questions_with_limited_num.where(level_of_chinese_character: level)
+    question_set = questions_with_limited_num_and_level.order("RANDOM()").limit(5)
+    @questions_with_choices = []
+    question_set.each do |question|
+      choices = generate_choices(question, type)
+      @questions_with_choices << { question: question, choices: choices }
+      previous_questions << question.id
+    end
+  end
+
   def generate_choices(question, type)
-      # 問題と同じレベルの他の熟語を3つ取得
-      # .where.notは()内に指定したidがquestion.idとは異なるものを取得する
     choices = ChineseCharacter.where(level_of_chinese_character: question.level_of_chinese_character)
                               .where.not(id: question.id)
                               .order("RANDOM()")
                               .limit(3)
-
-    # ラジオボタン用の選択肢を作成
-    # まず、answer_optionsの箱を作る[]、そこに、三項演算子を使ってreadとmeanの場合分けを行いながら、正解の選択肢(question.~)とそれ以外の選択肢(choice.~ <= choices)を追加する( << は箱(配列)[]への追加を意味する)
-    # 三項演算子とは、次のように一般化される
-    # (条件式 ? 真の場合 : 偽の場合)
-    type = params[:type]
     answer_options = []
     answer_options << (type == "read" ? question.reading_of_chinese_character : question.meaning_of_chinese_character)
     choices.each do |choice|
@@ -124,16 +107,6 @@ class QuestionsController < ApplicationController
     end
     answer_options.shuffle!
     return answer_options
-  end
-
-  def correct_params
-    if @type == "read"
-      params.require(:correct).permit(:correct_of_reading)
-    elsif @type == "mean"
-      params.require(:correct).permit(:correct_of_meaning)
-    else
-      # エラー処理を追加するか、デフォルトの設定を行います
-    end
   end
 
 end
