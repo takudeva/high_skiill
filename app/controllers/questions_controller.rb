d493bb3deef6df3d9f28cf37bc9627ab2962940f
takudeva <126333608+takudeva@users.noreply.github.com>
2023-10-21 08:30:55 +0000

    [Edit15]in making questions result

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -47,8 +47,6 @@ class QuestionsController < ApplicationController
     # @questionsから無作為に(order("RANDOM()"))、5問のみ(limit)抽出して@question_setに代入する
     @question_set = questions_with_limited_num_and_level.order("RANDOM()").limit(5)
 
-    # @question_set = @questions.order("RANDOM()").limit(5)
-
     # まず、@questions_with_choicesに空の配列(箱)[]を代入する
     # question_setに含まれるquestionそれぞれ(.each)に対し、choicesを用意する
     # @questions_with_choicesにオブジェクト(値はquestion,choices)を追加する
@@ -71,33 +69,31 @@ class QuestionsController < ApplicationController
   def answer
     @level = params[:level]
     @type = params[:type]
-    @correct = Correct.new
-    if @type == "read"
-      # correct_of_readingとして取得した値("#{qwc[:question].id}_#{choice}")を"_"を境に切り離し、前者をquestion_idに、後者をselected_choiceに代入
+      # indexとして取得した値("#{qwc[:question].id}_#{choice}")を5問分("0"-"4")、"_"を境に切り離し、前者をquestion_idに、後者をselected_choiceに代入
       # ChineseCharacterモデルからquestion_idと同じid、かつ、levelが受け取ったパラメータと同じものを探し、ローカル変数questionに代入
       # correct_choiceにquestion_idとlevelが一致する読み(question.reading_of_chinese_character)を代入しておく
       # selected_choice == correct_choiceかを判定 => 真の場合、is_correctに"true"代入、偽は"false"代入
       # @correct.correct_of_readingにはis_correct => 真偽値が代入される
-      question_id, selected_choice = params[:correct_of_reading].split("_")
+    ["0","1","2","3","4"].each do |i|
+      correct = Correct.new
+      question_id, selected_choice = params[i].split("_")
       question = ChineseCharacter.find_by!(id: question_id, level_of_chinese_character: @level)
-      correct_choice = question.reading_of_chinese_character
-      correct_choice == selected_choice ? is_correct = "true" : is_correct = "false"
-      @correct.correct_of_reading = is_correct
-    elsif @type == "mean"
-      question_id, selected_choice = params[:correct_of_meaning].split("_")
-      question = ChineseCharacter.find_by!(id: question_id, level_of_chinese_character: @level)
-      correct_choice = question.meaning_of_chinese_character
-      correct_choice == selected_choice ? is_correct = "true" : is_correct = "false"
-      @correct.correct_of_meaning = is_correct
+      if @type == "read"
+        correct_choice = question.reading_of_chinese_character
+        correct_choice == selected_choice ? is_correct = "true" : is_correct = "false"
+        correct.correct_of_reading = is_correct
+      elsif @type == "mean"
+        correct_choice = question.meaning_of_chinese_character
+        correct_choice == selected_choice ? is_correct = "true" : is_correct = "false"
+        correct.correct_of_meaning = is_correct
+      end
+      correct.user_id = current_user.id
+      correct.chinese_character_id = question.id
+      correct.save!
     end
-
-    # @correct.user_id = current_user.id # ログインユーザーのIDに適切な値を設定する
-    if @correct.save
-      flash[:success] = is_correct ? "正解です！" : "不正解です。"
-    else
-      flash[:error] = "解答を保存できませんでした。"
-    end
+    flash[:success] = "解答しました"
     redirect_to answer_questions_path
+
   end
 
   def result
